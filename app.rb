4b82ac56f18e4e391881a9719a40b2dc7bb8d9fd
Maher Hawash <gmhawash@gmail.com>
2017-04-18 12:36:50 -0700

    Don’t limit # requests.

--- app.rb
+++ app.rb
@@ -164,7 +164,8 @@ helpers do
 
   def requests
     @requests ||= begin
-      data = REDIS.lrange requests_key, 0, 9
+      size = REDIS.llen requests_key
+      data = REDIS.lrange requests_key, 0, size
       data.map! { |req| JSON.parse(req) }
       data.each { |req| req["time"] = Time.at(req["time"].to_f).utc }
     end
@@ -173,7 +174,7 @@ helpers do
   def store_request
     REDIS.multi do
       REDIS.lpush requests_key, package_request # append the request to the end
-      REDIS.ltrim requests_key, 0, 9 # restrict to 10 items (but trim the first part of the list, keeping the last 10)
+      #REDIS.ltrim requests_key, 0, 9 # restrict to 10 items (but trim the first part of the list, keeping the last 10)
       REDIS.expire requests_key, 172800 # delete all requests after 2 days: 2 * 24 * 60 * 60
     end
   end
